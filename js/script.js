3e00a68f41040546131459a7a5a9d05a93bbbcce
Keegan Lenz <Keegan.lenz@student.cart.org>
2020-05-16 15:10:36 -0700

    checks if user has made an input

--- js/script.js
+++ js/script.js
@@ -5,17 +5,38 @@ const addInput = (ev)=>{
     ev.preventDefault();
     let input = {
         id: date,
-        fname: document.getElementById('fname').value,
-        lname: document.getElementById('lname').value,
+        firstName: document.getElementById('fname').value,
+        lastName: document.getElementById('lname').value,
         email: document.getElementById('email').value,
         message: document.getElementById('message').value
     }
-    inputs.push(input);
-    document.forms[0].reset();
     
-    localStorage.setItem('MyInputList', JSON.stringify(inputs));
-    console.log(localStorage);
+    var firstName = document.getElementById('fname');
+    var lastName = document.getElementById('lname');
+    var email = document.getElementById('email');
+    var message = document.getElementById('message');
+
+    if(firstName.value.length == 0) {
+        window.alert('First name has not been filled in!');
+        
+    } else if (lastName.value.length == 0) {
+        window.alert('Last name has not been filled in!');
+
+    } else if (email.value.length == 0) {
+        window.alert('Email has not been filled in!');
+
+    } else if (message.value.length == 0) {
+        window.alert('You have not written a message!');
+
+    } else {
+
+        inputs.push(input);
+        document.forms[0].reset();
+        
+        localStorage.setItem('MyInputList', JSON.stringify(inputs));
+        window.alert("Your Message Has Been Sent");
     }
+}
 
     document.addEventListener('DOMContentLoaded', ()=>{
         document.getElementById('submit').addEventListener('click', addInput);
