51254b75c000de6ed245d287f1f817ead5243647
Keegan Lenz <Keegan.lenz@student.cart.org>
2020-05-17 13:41:54 -0700

    added image slider and animations

--- js/script.js
+++ js/script.js
@@ -1,3 +1,34 @@
+var slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("slides");
+  var dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}    
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";  
+  }
+  for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";  
+  dots[slideIndex-1].className += " active";
+}
+
+
+
+
+
 let inputs = [];
 let date = new Date().toISOString()
 
@@ -18,7 +49,7 @@ const addInput = (ev)=>{
 
     if(firstName.value.length == 0) {
         window.alert('First name has not been filled in!');
-        
+
     } else if (lastName.value.length == 0) {
         window.alert('Last name has not been filled in!');
 
